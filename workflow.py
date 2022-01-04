f3e4495e2fe4e616146d3ab97be7793e3809f74b
trif0lium <gh@fleeingsunlight.dev>
2022-01-04 14:23:16 +0700

    feat(workflow.py): sleep() -> time.sleep()

--- workflow.py
+++ workflow.py
@@ -1,5 +1,6 @@
 import numpy as np
 import datetime
+import time
 
 shape, scale = 36, 5/6
 s = np.random.gamma(shape, scale, 1000)
@@ -8,4 +9,4 @@ while True:
     for i in s:
         print(datetime.datetime.now())
         print(s)
-        sleep(s)
+        time.sleep(s)
