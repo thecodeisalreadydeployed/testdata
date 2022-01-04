b0e29cb52d3125afb3effa97ef5c3563211285b1
trif0lium <gh@fleeingsunlight.dev>
2022-01-04 14:21:46 +0700

    feat(workflow.py): for i in s

--- workflow.py
+++ workflow.py
@@ -1,4 +1,11 @@
 import numpy as np
+import datetime
+
 shape, scale = 36, 5/6
 s = np.random.gamma(shape, scale, 1000)
-print(s)
+
+while True:
+    for i in s:
+        print(datetime.datetime.now())
+        print(s)
+        sleep(s)
