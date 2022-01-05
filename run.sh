b8986ed0db960e2dbade404a6b1c9389ac1d1f77
trif0lium <gh@fleeingsunlight.dev>
2022-01-05 13:52:24 +0700

    fix(run): sed -> echo

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cd workspace
 ls
 
 git switch testdata
-sed -i "1s;^;// $(date)\n\n" nx/apps/nest-a/src/main.ts
+echo -e "// $(date)\n\n$(cat nx/apps/nest-a/src/main.ts)" > nx/apps/nest-a/src/main.ts
 cat nx/apps/nest-a/src/main.ts
