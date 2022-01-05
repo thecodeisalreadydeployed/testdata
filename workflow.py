a94e525e77a4427fc613a960f1058eab69e72198
trif0lium <gh@fleeingsunlight.dev>
2022-01-05 12:02:56 +0700

    feat(workflow): subprocess.run()

--- workflow.py
+++ workflow.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime
 import time
+import subprocess
 
 shape, scale = 36, 5/6
 s = list(np.random.gamma(shape, scale, 5))
@@ -11,3 +12,5 @@ for i in s:
 
     # TODO(trif0lium): change i/2 to i
     time.sleep(i/2)
+
+    subprocess.run(["git", "config", "user.name"], capture_output=True)
