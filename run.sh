e3bccecf3f7ed434668fd561ceb4a23c277aaa23
trif0lium <gh@fleeingsunlight.dev>
2022-01-05 13:57:46 +0700

    feat(run): date --iso-8601

--- run.sh
+++ run.sh
@@ -5,5 +5,8 @@ cd workspace
 ls
 
 git switch testdata
-echo -e "// $(date)\n\n$(cat nx/apps/nest-a/src/main.ts)" > nx/apps/nest-a/src/main.ts
+echo -e "// $(date --iso-8601)\n\n$(cat nx/apps/nest-a/src/main.ts)" > nx/apps/nest-a/src/main.ts
 cat nx/apps/nest-a/src/main.ts
+
+git add nx/apps/nest-a/src/main.ts
+git commit -m "$(date --iso-8601)"
