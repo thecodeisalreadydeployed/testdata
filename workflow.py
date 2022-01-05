9d836c50a73f52e84d4a9db6a0fb8ba8fda67794
trif0lium <gh@fleeingsunlight.dev>
2022-01-05 14:49:08 +0700

    chore(workflow): set size to 2 for np.random.gamma()

--- workflow.py
+++ workflow.py
@@ -6,14 +6,14 @@ import subprocess
 shape, scale = 36, 5/6
 
 # TODO(trif0lium): set size to 5
-s = list(np.random.gamma(shape, scale, 1))
+s = list(np.random.gamma(shape, scale, 2))
 
 for i in s:
     print(datetime.datetime.now())
     print(i)
 
     # TODO(trif0lium): change i/2 to i
-    time.sleep(i/2)
+    time.sleep(i * 60 /2)
 
     process = subprocess.run(["/bin/sh", "run.sh"], capture_output=True)
     print(process.stdout)
