ba9e50de72df53b4d061dbda5281753af497087a
trif0lium <gh@fleeingsunlight.dev>
2022-01-05 14:43:03 +0700

    chore(run): echo -> sed

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ cd workspace
 ls
 
 git switch testdata
-echo "// $(date --iso-8601=seconds)\n\n$(cat nx/apps/nest-a/src/main.ts)" > nx/apps/nest-a/src/main.ts
+# echo "// $(date --iso-8601=seconds)\n\n$(cat nx/apps/nest-a/src/main.ts)" > nx/apps/nest-a/src/main.ts
+sed -i "1s/.*/\/\/ $(date --iso-8601=seconds)/" ./nx/apps/nest-a/src/main.ts
 cat nx/apps/nest-a/src/main.ts
 
 git add nx/apps/nest-a/src/main.ts
 git commit -m "$(date --iso-8601=seconds)"
-git push
+# git push
